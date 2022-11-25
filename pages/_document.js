d0705df10149b593a45242bdb387d586321461f3
unknown <ricardomarcal02@hotmail.com>
2022-11-25 20:48:23 -0300

    pwa + fixes + manifest.json

--- pages/_document.js
+++ pages/_document.js
@@ -5,6 +5,12 @@ export default function Document() {
     <Html lang='pt-br'>
       <Head>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" crossOrigin="anonymous" />
+        <meta charset="utf-8" />
+        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="msapplication-TileColor" content="#ffffff" />
+        <meta name="theme-color" content="#ffffff" />
         <link rel="manifest" href="/manifest.json" />
       </Head>
       <body>
