b768fc1ddfc80605a8eb38e9d087cc9ae428541e
unknown <ricardomarcal02@hotmail.com>
2022-11-25 19:18:19 -0300

    questions and answers + visual fixes

--- pages/_document.js
+++ pages/_document.js
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang='pt'>
+    <Html lang='pt-br'>
       <Head>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" crossOrigin="anonymous" />
       </Head>
---
+++ public/options.ts
@@ -0,0 +1,44 @@
+export const options = [
+    {
+      question: "Olá, desejaria fazer um atendimento na nossa clínica?",
+      input: "select",
+      answers: [
+        {value: "Sim", goto: 1},
+        {value: "Não", goto: 0}
+      ]
+    },
+    {
+      question: "Por qual especialista você deseja ser atendido(a)?",
+      input: "select",
+      answers: [
+        {value: "Neurologista", goto: 2},
+        {value: "Cardiologista", goto: 2},
+        {value: "Psiquiatra", goto: 2},
+        {value: "Fonoaudiólogo", goto: 2},
+        {value: "Dermatologista", goto: 2}
+      ]
+    },
+    {
+      question: "Informe a data em que você deseja agendar.",
+      input: "date",
+      answers: [
+        {value: "", goto: 3},
+      ]
+    },
+    {
+      question: "Informe o horário que você deseja ser atendido.",
+      input: "select",
+      answers: [
+        {value: "7:00 da manhã.", goto: 4},
+        {value: "13:30 da tarde.", goto: 4},
+        {value: "Agendar outro dia.", goto:2}
+      ]
+    },
+    {
+      question: "Ok! Informe seu CPF para finalizar seu agendamento.",
+      input: "cpf",
+      answers: [
+        {value: "", goto: 5},
+      ]
+    },
+  ];
